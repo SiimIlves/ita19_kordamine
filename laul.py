9d54132e5ddee3f20fd55a5f926c9ac22932a6fd
Siim Ilves <siim.ilves.001@khk.ee>
2020-09-02 10:59:04 +0300

    Lisati albumite nimekiri

---
+++ laul.py
@@ -0,0 +1,10 @@
+class Laul:
+    """Klass, mis kirjeldab laulu
+
+    Omadused (Atribuudid)
+    pealkiri (str): laulu pealkiri
+    laulja (str): laulu laulja (grupp)"""
+
+    def __init__(self, p, l):
+        self.pealkiri = p
+        self.laulja = l
---
+++ menu.py
@@ -0,0 +1,4 @@
+from laul import Laul
+
+laul1 = Laul("Für Oksana", "Nublu")
+print(laul1.pealkiri, laul1.laulja)
